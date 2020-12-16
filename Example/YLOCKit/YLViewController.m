b7f719a8617373283ccc5a83587ef202a7ac50f5
包燕龙 <>
2020-12-16 16:29:42 +0800

    Bump version to

--- Example/YLOCKit/YLViewController.m
+++ Example/YLOCKit/YLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "YLViewController.h"
+#import <YLOCKit.h>
 
 @interface YLViewController ()
 
@@ -18,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [YLPublicMethod compareVersionWithOnlineVersion:@"1.0.2"];
 }
 
 - (void)didReceiveMemoryWarning
